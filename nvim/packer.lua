9d17d7beab1444f890fdffcb257d5d7284d56ecb
Andres Lamilla <andres.lamilla@tropipay.com>
2024-03-04 12:24:24 +0100

    nvim config packer to lazy WIP

--- nvim/packer.lua
+++ nvim/packer.lua
@@ -13,17 +13,6 @@ return require('packer').startup(function(use)
       requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  -- use({
-  --     'rose-pine/neovim',
-  --     as = 'rose-pine',
-  --     config = function()
-  --         vim.cmd('colorscheme rose-pine')
-  --     end
-  -- })
-  -- use 'olimorris/onedarkpro.nvim' -- Colorscheme
-  use { "catppuccin/nvim", as = "catppuccin" }
-
-
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
   use('nvim-lua/plenary.nvim')
