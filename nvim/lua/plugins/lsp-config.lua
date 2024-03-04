c8ea697f8b0e35017983a360eba87a928441cb51
Andres Lamilla <andres.lamilla@tropipay.com>
2024-03-04 17:18:17 +0100

    Add lsp keymaps

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,10 +1,10 @@
 return {
   {
-    'williamboman/mason.nvim',
-    config = true
+    "williamboman/mason.nvim",
+    config = true,
   },
   {
-    'williamboman/mason-lspconfig.nvim',
+    "williamboman/mason-lspconfig.nvim",
     opts = {
       ensure_installed = {
         "lua_ls",
@@ -14,28 +14,42 @@ return {
         "pyright",
         "gopls",
         "bashls",
-      }
+      },
     },
-    config = true
+    config = true,
   },
   {
-    'neovim/nvim-lspconfig',
+    "neovim/nvim-lspconfig",
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require('lspconfig')
+      local lspconfig = require("lspconfig")
 
       lspconfig.lua_ls.setup({})
       lspconfig.tsserver.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
 
+      -- Global mappings.
+      -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+      vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+
+      -- see :help lsp-zero-keybindings
+      -- to learn the available actions
+      -- lsp_zero.default_keymaps({ buffer = bufnr })
+      -- lsp_zero.buffer_autoformat()
+
+      -- Buffer local mappings.
+      -- See `:help vim.lsp.*` for documentation on any of the below functions
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", function()
         vim.cmd.vsplit()
         vim.lsp.buf.definition()
       end, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end
-  }
+    end,
+  },
 }
